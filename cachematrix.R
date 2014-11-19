7aaf78b61b22af57b9c999256a5317ddbe2b9818
MohithKumar <mohithkumar@hotmail.com>
2014-11-19 19:32:41 +0000

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,39 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  ## Dummy comment
+  m <- NULL
+  
+  ## set(), get(), setsolve(), getsolve() are all internal functions
+  set <- function(y) {
+    ## 'x' and 'm' are cached variables
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  ## Dummy comment
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  ## 'm' is the internal variable here.
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  ## If the internal variable 'm' is null, then its inverse is calculated
+  m <- solve(data, ...)
+  
+  x$setsolve(m)
+  ## Return m
+  m
 }
