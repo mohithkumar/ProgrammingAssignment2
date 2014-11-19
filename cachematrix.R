cc83fcbeb8160835c622daac415d39c3f254df2d
MohithKumar <mohithkumar@hotmail.com>
2014-11-19 18:33:55 +0000

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,13 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Dummy comment
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  ## Dummy comment
         ## Return a matrix that is the inverse of 'x'
 }
